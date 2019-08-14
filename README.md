e6d3c6c318d4bbc80b04dd05a1044328ef2e02db
PBearson <peabryan95@gmail.com>
2019-08-14 14:31:56 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+###ESP-IDF Stable Initializer###
+
+Set up ESP-IDF for your workspace. Simply run "bash setup.sh" to install everything. The default install location is "$HOME/esp", but this can be changed in the script. 
+
+The script works successfully on x64 architectures and Debian/Ubuntu distributions. Support for other environments may come later.
+
+
---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script will setup the most stable release of ESP-IDF
+# The environment is installed in the ~/esp directory.
+# Environment variables are set using scripts in the /etc/profile.d/ directory.
+
+# Change this to manually set the install location
+ESP_DIR="$HOME/esp"
+
+# Install deps
+sudo apt-get --assume-yes install gcc git wget make libncurses-dev flex bison gperf python python-pip python-setuptools python-serial python-cryptography python-future aria2
+
+# Download toolchain
+mkdir -p $ESP_DIR
+cd $ESP_DIR
+aria2c -x 16 https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+tar -xvf xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+rm xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
+
+# Setup path to toolchain
+export PATH="$ESP_DIR/xtensa-esp32-elf/bin:$PATH"
+echo 'export PATH="$ESP_DIR/xtensa-esp32-elf/bin:$PATH"' | sudo tee /etc/profile.d/setup_xtensa_path.sh
+bash /etc/profile.d/setup_xtensa_path.sh
+
+# Get ESP-IDF
+git clone -b v3.2.2 --recursive https://github.com/espressif/esp-idf.git
+
+# Setup path to ESP-IDF
+export IDF_PATH="$ESP_DIR/esp-idf"
+echo 'export IDF_PATH="$ESP_DIR/esp-idf"' | sudo tee /etc/profile.d/setup_idf_path.sh
+bash /etc/profile.d/setup_idf_path.sh
+
+# Install python deps
+python -m pip install --user -r $IDF_PATH/requirements.txt
+
